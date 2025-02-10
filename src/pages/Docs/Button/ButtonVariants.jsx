c63fe5c55ed8c2da937183b14029d9cd60236e52
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-10 10:09:37 +0530

    Update

--- src/pages/Docs/Button/ButtonVariants.jsx
+++ src/pages/Docs/Button/ButtonVariants.jsx
@@ -3,6 +3,15 @@ import React from 'react'
 const ButtonVariants = () => {
   return (
     <div>
+        <div className="flex my-4">
+            <div className="px-2"><button className="jkbtn jkbtn-blue"> Button Blue </button></div>
+            <div className="px-2"><button className="jkbtn jkbtn-gray"> Button Gray </button></div>
+            <div className="px-2"><button className="jkbtn jkbtn-green"> Button Green </button></div>
+            <div className="px-2"><button className="jkbtn jkbtn-red"> Button Red </button></div>
+            <div className="px-2"><button className="jkbtn jkbtn-yellow"> Button Yellow </button></div>
+            <div className="px-2"><button className="jkbtn jkbtn-info"> Button Info </button></div>
+        </div>
+
         <pre>
             <code>
                 <span className="tag">&lt;button</span> <span className="attribute">class=</span><span className="value">"jkbtn jkbtn-blue"</span><span className="tag">&gt;</span>Button Blue <span className="tag">&lt;/button&gt;</span><br />
