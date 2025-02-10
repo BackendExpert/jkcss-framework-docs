97fa668836565c85a29b63ed34f93d3170d49ac7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-10 08:05:49 +0530

    Update

---
+++ src/components/NPMcode/Npmcode.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Npmcode = ({ npmcode }) => {
+  return (
+    <div className='bg-gray-200 w-full rounded shadow-md px-4'>
+        <p className="py-4 text-purple-500">{npmcode}</p>
+    </div>
+  )
+}
+
+export default Npmcode
--- src/pages/Docs/Button/ButtonMain.jsx
+++ src/pages/Docs/Button/ButtonMain.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Npmcode from '../../../components/NPMcode/Npmcode'
 
 const ButtonMain = () => {
   return (
@@ -14,6 +15,21 @@ const ButtonMain = () => {
 
         <p className="text-gray-500 py-4">Ensure you have installed JKCSS Framework before using the Button component: <a href="/GetStarted" target='_blank' className='pl-2 text-purple-600 duration-500 hover:font-semibold hover:pl-4'>Installation Guide</a></p>
 
+        <div className="">
+            <Npmcode npmcode={'npm i @jehankandy/jkcss'}/>
+        </div>
+
+        <p className="text-gray-500 py-4">Or include it via CDN:</p>
+
+        <div className="">
+            <Npmcode npmcode={'https://cdn.jsdelivr.net/npm/@jehankandy/jkcss@6.0.0/src/jkcss.min.css'}/>
+        </div>
+
+        <div className="py-4 text-gray-500">
+            <h1 className="text-purple-500 uppercase font-semibold text-xl">Usage</h1>
+
+            <p className="py-2">Use the .btn class to create a basic button:</p>
+        </div>
 
     </div>
   )
